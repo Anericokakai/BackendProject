972201a726918f6bd512346fd88a6ffca81bbb32
Anericokakai <challykiddoh@gmail.com>
2023-10-23 17:42:08 +0300

    initalisation of the project

---
+++ src/main/java/com/backendJava/backendTutor/BackendTutorApplication.java
@@ -0,0 +1,13 @@
+package com.backendJava.backendTutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendJava/backendTutor/BackendTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendJava.backendTutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
